29b1c5da735f5ff3fa7decb5155482d86c476ab3
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-05-05 19:56:32 -0700

    add jQuery ready function to fix logic

--- js/deck-scripts.js
+++ js/deck-scripts.js
@@ -1,11 +1,13 @@
-let suits = ['clubs', 'diamonds', 'spades', 'hearts'];
-let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'king', 'queen'];
-
-suits.forEach(function(suit) {
-  cards.forEach(function(card) {
-    $("#all").append("<li>" + card + " of " + suit +"</li>");
-  });
-});
+$(document).ready(function() {
+  let suits = ['clubs', 'diamonds', 'spades', 'hearts'];
+  let cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'king', 'queen'];
+  
+  suits.forEach(function(suit) {
+    cards.forEach(function(card) {
+      $("#all").append("<li>" + card + " of " + suit +"</li>");
+    });
+  });  
+})
 
 // let pickCard = () => {
 //   suits.forEach(suit => {
